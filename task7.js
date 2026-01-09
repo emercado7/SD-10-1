6f39d0bc32c011a6571426eb182031b82b222c16
Esteban Mercado <esteban.mercadom99@gmail.com>
2026-01-08 22:42:02 -0600

    Se agregó switch para evaluar el estatus de la calificación

--- task7.js
+++ task7.js
@@ -1,3 +1,16 @@
-export function rubricPerfect() {
+export function rubricPerfect(value) {
+    const grade = Number(value)
+    switch (grade) {
+        case (grade < 5 ? grade : false):
+            return "Fail"
+        case (grade < 9 ? grade : false):
+            return "Pass"
+        case (grade < 11 ? grade : false):
+            return "Excellent"
+        case (grade === 11 ? grade : false):
+            return "Perfect"
+        default:
+            return "Undefined"
+    }
 
 }
